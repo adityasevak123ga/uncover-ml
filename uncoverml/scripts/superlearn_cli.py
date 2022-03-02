ea099fd69dd5c2b116c609c3bb1d224357a4431a
snazzybloke <snazzybloke@yahoo.com>
2022-03-02 11:44:21 +1100

    superlearn learn/predict skeleton works

--- uncoverml/scripts/superlearn_cli.py
+++ uncoverml/scripts/superlearn_cli.py
@@ -8,14 +8,22 @@ import logging
 import pickle
 from pathlib import Path
 import os
-import re
 import warnings
 
 import click
 import yaml
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+from revrand.metrics import smse
 
 import uncoverml.config
 import uncoverml.scripts
+from uncoverml.geoio import RasterioImageSource, ImageWriter, get_image_spec
+from uncoverml.features import extract_subchunks
 
 _logger = logging.getLogger(__name__)
 warnings.filterwarnings(action='ignore', category=FutureWarning)
@@ -54,31 +62,28 @@ def main(yaml_file, partitions):
                 _logger.error("Couldn't parse the yaml file. Ensure you've provided the correct "
                               "file as config file and that the YAML is valid.")
     learn_lst = _grp(s, 'learning', "'learning' block must be provided when superlearning.")
-    # with open("./File.yaml", 'w') as f:
-        # yaml.dump(s['learning'], f, default_flow_style=False, sort_keys=False)
+
     s.pop("learning")
     ddd = {}
+    model_lst = []
+    df = pd.DataFrame()
     for alg in learn_lst:
         ddd.update({"learning": alg})
         ddd.update(s)
-        ddd["output"]["directory"] = f"./{alg['algorithm']}_out"
-        ddd["output"]["model"] = f"./{alg['algorithm']}_out/{alg['algorithm']}.model"
-        ddd["pickling"]["covariates"] = f"./{alg['algorithm']}_out/features.pk"
-        ddd["pickling"]["targets"] = f"./{alg['algorithm']}_out/targets.pk"
-        with open(f"{alg['algorithm']}.yml", 'w') as yout:
+        alg_outdir = f"./{alg['algorithm']}_out"
+        ddd["output"]["directory"] = alg_outdir
+        ddd["output"]["model"] = alg_outdir + f"/{alg['algorithm']}.model"
+        ddd["pickling"]["covariates"] = alg_outdir + "/features.pk"
+        ddd["pickling"]["targets"] = alg_outdir + "/targets.pk"
+        alg_yaml = f"./{alg['algorithm']}.yml"
+        with open(alg_yaml, 'w') as yout:
             yaml.dump(ddd, yout, default_flow_style=False, sort_keys=False)
-        uncoverml.scripts.learn.callback(f"{alg['algorithm']}.yml", partitions)
+        uncoverml.scripts.learn.callback(alg_yaml, partitions)
 
-        # predicting
-        # pb = _grp(s, 'prediction', "'prediction' block must be provided.")
-        # self.geotif_options = pb.get('geotif', {})
-        # self.quantiles = _grp(pb, 'quantiles', "'quantiles' must be provided as part of prediction block.")
-        # self.outbands = _grp(pb, 'outbands', "'outbands' must be provided as part of prediction block.")
-        # self.thumbnails = pb.get('thumbnails', 10)
-        # self.bootstrap_predictions = pb.get('bootstrap')
+        model, feature_sets, final_transform = _load_model(ddd["output"]["model"])
+        model_lst.append(model)
 
         retain = None
-
         mb = s.get('mask')
         if mb:
             mask = mb.get('file')
@@ -88,16 +93,101 @@ def main(yaml_file, partitions):
         else:
             mask = None
 
-        # if self.krige:
-            # Todo: don't know if lon/lat is compulsory or not for kriging
-            # self.lon_lat = s.get('lon_lat')
-        # else:
-            # self.lon_lat = None
-
         try:
-            uncoverml.scripts.predict.callback(f"{alg['algorithm']}.yml", partitions, mask, retain)
+            uncoverml.scripts.predict.callback(alg_yaml, partitions, mask, retain)
         except TypeError:
             _logger.error(f"Learner {alg} cannot predict")
-    # uncoverml.scripts.cli.commands['learn']("./aaa.yml", partitions)
-    # uncoverml.scripts.learn.callback("./aaa.yaml", partitions)
+
+        pred = pd.read_csv(alg_outdir + f"/{alg['algorithm']}_crossval_results.csv")
+        pred.rename(columns={'# y_true': 'y_true', 'y_pred': alg['algorithm']}, inplace=True)
+        if df.empty:
+            df = pred
+        else:
+            df = df.merge(pred, on=['y_true', 'x', 'y'])
+
+    df.drop(columns=['x', 'y'], inplace=True)
+    _logger.info(f"Base model training set predictions:\n{df.head()}")
+    _logger.info("..........")
+    _logger.info(df.tail())
+    y = df['y_true'].values
+    X = df.drop(columns='y_true').values
+    super_cv(X, y)
+    s_model = super_train(X, y)
+    super_predict(s_model, learn_lst, model_lst[-1], alg_yaml)
     return
+
+
+def super_cv(X, y, n_splits=5):
+    """
+    ...
+    """
+
+    kfold = KFold(n_splits=n_splits)
+    meta_X = []
+    meta_y = []
+
+    for train_ix, test_ix in kfold.split(X):
+        y_fold = []
+        train_X, test_X = X[train_ix], X[test_ix]
+        train_y, test_y = y[train_ix], y[test_ix]
+        sm_fold = fit_super_model(train_X, train_y)
+        meta_y.extend(sm_fold.predict(test_X))
+    _logger.info("SUPER CV smse: {smse(y, meta_y)}")
+    _logger.info("SUPER CV r2: {r2_score(y, meta_y)}")
+    plt.scatter(y, meta_y)
+    plt.xlabel("True target")
+    plt.ylabel("Super-learner predicted");
+    plt.savefig("Super_cv_predicted.png")
+    plt.close()
+
+
+def super_train(X, y):
+    """
+    ...
+    """
+
+    s_model = fit_super_model(X, y)
+    _logger.info(f"{s_model.coef_}")
+    print(s_model.intercept_)
+    y_super = s_model.predict(X[:, :])
+    print("smse:", smse(y, y_super))
+    print("r2:", r2_score(y, y_super))
+    plt.scatter(y, y_super)
+    plt.xlabel("True target")
+    plt.ylabel("Super-learner predicted")
+    plt.savefig("Super_train_predicted.png")
+    plt.close()
+    return s_model
+
+
+def super_predict(s_model, learn_lst, alg_model, alg_yaml):
+    """
+    ...
+    """
+
+    arr_lst = []
+    for alg in learn_lst:
+        alg_outdir = f"./{alg['algorithm']}_out"
+        tif = alg_outdir + f"/{alg['algorithm']}_prediction.tif"
+        img = RasterioImageSource(tif)
+        arr_lst.append(extract_subchunks(img, 0, 1, 0)[:, 0, 0, 0])
+
+    X_all = np.column_stack(arr_lst)
+    y_all = s_model.intercept_ + X_all.dot(s_model.coef_)
+    _logger.debug("Feature-target shapes: ", X_all.shape, y_all.shape)
+
+    alg_config = uncoverml.config.Config(alg_yaml)
+    img_shape, img_bbox, img_crs = get_image_spec(alg_model, alg_config)
+    img_out = ImageWriter(img_shape, img_bbox, img_crs, 1, "New.tif", 1, band_tags=["Prediction"])
+    img_out.write(y_all.reshape(-1, 1), 0)
+
+
+def _load_model(model_file):
+    with open(model_file, 'rb') as f:
+        return pickle.load(f)
+
+
+def fit_super_model(X, y):
+    s_model = LinearRegression()
+    s_model.fit(X, y)
+    return s_model
